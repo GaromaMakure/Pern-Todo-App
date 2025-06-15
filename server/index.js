e897af8b83635bc9b75bb549de22ab6fc8207a18
GaromaMakure <garomamak11@gmail.com>
2025-06-15 11:01:25 -0700

    adding routes

--- server/index.js
+++ server/index.js
@@ -11,18 +11,37 @@ app.use(express.json());
 // create a todo
 app.post("/todos", async (req, res) => {
   try {
-    const{description} = req.body;
-    
-    res.status(200).json({ message: "Todo received", body: req.body });
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
   }
 });
 
 // get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // update a todo
 // delete a todo
